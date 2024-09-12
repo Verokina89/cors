1ba56dfa5ef9e042aa5a8bd650970030ae0906bb
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-09-12 18:05:06 +0200

    app.js ok

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-//Servidor creado. 
+//RUTAS 
 const express = require ('express')
 const app = express()
 const axios = require('axios')
 const cors = require('cors')
 
+const API = 'https://rickandmortyapi.com/api/character/?name='
 //Middleware cors para peticiones del FRONT.
 app.use(cors()); 
 
@@ -14,9 +15,37 @@ app.use(cors());
     
 // })
 
+//Middleware para obtener todos los personajes
+app.get('/characters', async (req, res) => {
+    try {
+        const response = await axios.get(API)
+        const charactersName = await response.data.results
+        res.json(charactersName) //devuelve lista de personajes
+    }catch (error) {
+        console.log('Error al obtener personajes', error)
+        res.status(404).json({message: 'Error al obtener personajes'})
+    }
+})
 
+//Middleware para obtener personajes por nombre.
+app.get('characters/:name', async (res, req) => {
+    const name = req.params.name
+    
+    try{
+        const response = await axios.get(`${API}${name}`)
+        const characterName = await response.data.results[0]
+            res.json(characterName)
+    }catch (error) {
+        console.log('Error al obtener personaje', error)
+        res.status(404).json({message: 'Personaje no encontrado'})
+    }
+    
+})
 
-const PORT = 3000;
+app.use((req,res) => {
+    res.send('<h1>⚠️Pagina no encontrada⚠️')
+})
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor en el puerto http://localhost:3000/characters/Rick`)
+    console.log(`Servidor en el puerto http://localhost:${PORT}/characters`)
 })
